6dbc5ce42949b69e1abff69edd164283808dafa5
LoudStatic13 <jamespeacock13@yahoo.com>
2023-04-16 01:33:44 -0400

    Update application.py

--- application.py
+++ application.py
@@ -1,6 +1,7 @@
-from flask import Flask
-app = Flask(__name__)
+from flask import Flask, request
 from flask_sqlalchemy import SQLAlchemy
+app = Flask(__name__)
+
 
 app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///data.db"
 db = SQLAlchemy(app)
@@ -30,3 +31,25 @@ def get_books():
 
     return {"books": output}
 
+
+@app.route("/books/<id>")
+def get_book(id):
+    book = Book.query.get_or_404(id)
+    return {"book_name": book.book_name, "author": book.author, "publisher": book.publisher}
+
+@app.route("/books", methods=["POST"])
+def add_book():
+    book = Book(book_name=request.json["book_name"], author=request.json["author"], publisher=request.json["publisher"])
+    db.session.add(book)
+    db.session.commit()
+    return{"id": book.id}
+
+
+@app.route("/books/<id>", methods=["DELETE"])
+def delete_book(id):
+    book = Book.query.get(id)
+    if book is None:
+        return{"error": "not found"}
+    db.session.delete(book)
+    db.session.commit()
+    return{"message": "deleted"}
